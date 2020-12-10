9dc8461bbd21a684d5eb4e0e64c60b436091a048
Estibaliz Barato <estibalizbarato@gmail.com>
2020-12-10 14:22:42 +0100

    add ARIA label to jsx

--- src/components/Product-Slides.js
+++ src/components/Product-Slides.js
@@ -5,35 +5,39 @@ import images2 from "../images/Mesa-negra.png";
 const Slides = () => {
   return (
     <React.Fragment>
-      <h5>mater</h5>
-      <button>burger</button>
-      <ul>
-        <li>
-          <article>
-            <small>autor</small>
-            <h4>Forniture name</h4>
-            <p>
-              Lorem fistrum a peich labore a wan. Pecador exercitation ullamco
-              velit por la gloria de mi madre veniam incididunt ese que llega
-              papaar papaar incididunt pecador.{" "}
-            </p>
-            <img src={images1} alt="imagen silla negra" />
-          </article>
-          <button>Product Details</button>
-        </li>
-        <li>
-          <article>
-            <small>autor</small>
-            <h4>Forniture name</h4>
-            <p>
-              Lorem fistrum a peich labore a wan. Pecador exercitation ullamco
-              velit por la gloria de mi madre veniam incididunt ese que llega
-              papaar papaar incididunt pecador.{" "}
-            </p>
-            <img src={images2} alt="imagen silla negra" />
-          </article>
-        </li>
-      </ul>
+      <main role="main" aria-label="products presentation">
+        <h5>mater</h5>
+        <button type="button" aria-label="close this slide">
+          burger
+        </button>
+        <ul aria-label="content-info">
+          <li>
+            <article aria-label="product general description">
+              <small>autor</small>
+              <h4>Forniture name</h4>
+              <p>
+                Lorem fistrum a peich labore a wan. Pecador exercitation ullamco
+                velit por la gloria de mi madre veniam incididunt ese que llega
+                papaar papaar incididunt pecador.{" "}
+              </p>
+              <img src={images1} alt="black wooden chair" />
+            </article>
+            <button>Product Details</button>
+          </li>
+          <li>
+            <article aria-label="product general description">
+              <small>autor</small>
+              <h4>Forniture name</h4>
+              <p>
+                Lorem fistrum a peich labore a wan. Pecador exercitation ullamco
+                velit por la gloria de mi madre veniam incididunt ese que llega
+                papaar papaar incididunt pecador.{" "}
+              </p>
+              <img src={images2} alt="black wooden table" />
+            </article>
+          </li>
+        </ul>
+      </main>
     </React.Fragment>
   );
 };
