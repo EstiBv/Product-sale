fbe4302ec1f7d8d81d7febf2327da92c5422e43c
Estibaliz Barato <estibalizbarato@gmail.com>
2020-12-11 12:27:55 +0100

    menu component, add style first step

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,16 @@ import "../stylesheets/Header.scss";
 const Header = () => {
   return (
     <header aria-label="header" className="header">
-      <h1 className="header__title">mater</h1>
-      <button
-        type="button"
-        aria-label="close this slide"
-        className="header__btn"
-      >
-        burger
-      </button>
+      <nav className="header__nav-container">
+        <h1 className="header__title">mater</h1>
+        <button
+          type="button"
+          aria-label="close this slide"
+          className="header__btn"
+        >
+          burger
+        </button>
+      </nav>
     </header>
   );
 };
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,26 @@
 import React from "react";
 import images3 from "./images/Sofa-blanco.png";
+import "../stylesheets/Menu.scss";
 
 class Menu extends React.Component {
   render() {
     return (
       <>
-        <div className="slide">
-          <section aria-label="areas list">
-            <ul>
+        <div className="menu-container">
+          <section
+            aria-label="areas list"
+            className="menu-container__list-collapsable"
+          >
+            <ul className="menu-container__list">
               Collection
               <li>
                 Furniture
                 <div>
-                  <img src={images3} alt="sofa blanco individual" />
+                  <img
+                    src={images3}
+                    alt="sofa blanco individual"
+                    className="image3-sofaBlanco"
+                  />
                 </div>
               </li>
               <li>Lighthing</li>
@@ -22,7 +30,7 @@ class Menu extends React.Component {
             <ul>Crafmanship</ul>
             <ul>Ethics</ul>
           </section>
-          <footer>
+          <footer className="menu-container__footer">
             <table>
               <thead>
                 <tr>
