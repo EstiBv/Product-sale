4c703dd86702a3d23a3ceffe4b992cc91f284921
Estibaliz Barato <estibalizbarato@gmail.com>
2020-12-21 19:14:14 +0100

    add product styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,21 +5,21 @@ import "../stylesheets/Header.scss";
 const Header = () => {
   return (
     <header aria-label="header" className="header">
-      <nav className="header__nav-container">
-        <Link to="/">
-          <h1 className="header__title">mater</h1>
+      <nav className="header__nav">
+        <Link to="/" className="header__nav--mater">
+          <h1 className="header__title">Mater</h1>
         </Link>
-        <Link to="/products/">
-          <div id="burguer" className="burguer">
-            <span></span>
-            <span></span>
-          </div>
+        <Link to="/products/" className="header__nav--burguer">
           <button
             type="button"
             aria-label="close this slide"
-            className="header__btn"
+            className="header__nav--burguer-btn"
           >
-            burger
+            <div id="burguer" className="burguer">
+              <span className="div1"></span>
+              <span className="div2"></span>
+            </div>
+            Burger
           </button>
         </Link>
       </nav>
--- src/components/slide01/Product.js
+++ src/components/slide01/Product.js
@@ -1,22 +1,28 @@
 import React from "react";
 import "../../stylesheets/App.scss";
-
+import "../../stylesheets/Product.scss";
 const Product = (props) => {
   const eachProduct = props.infoProducts.map((a, i) => {
     return (
-      <section
-        aria-label="content-info"
-        className="slide-container"
-        key={a.id}
-        id={i}
-      >
-        <div className="slide">
-          <article aria-label="slide__product-description" className="slide">
-            <li>
-              <small>{a.author}</small>
-              <h4>{a.name}</h4>
-              <p className="paragraph">{a.description}</p>
-              <img src={a.img} alt="imagen de producto" />
+      <section aria-label="content-info" key={a.id} id={i} className="slide">
+        <div className="container">
+          <article
+            aria-label="slide__product-description"
+            className="container__products"
+          >
+            <li className="container__data">
+              <div className="container__data--text">
+                <small>{a.author}</small>
+                <h4>{a.name}</h4>
+                <p className="paragraph">{a.description}</p>
+              </div>
+              <div>
+                <img
+                  src={a.img}
+                  alt="imagen de producto"
+                  className="container__data--img"
+                />
+              </div>
             </li>
           </article>
         </div>
